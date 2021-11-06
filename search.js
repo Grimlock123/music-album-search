1982b2282b56fda07686b674ccbc002273422493
grimlock123 <barry.mouhamadou6@gmail.com>
2021-11-06 08:19:29 -0400

    2nd iteration

--- search.js
+++ search.js
@@ -11,20 +11,30 @@ userInput.addEventListener('submit', formSubmited)
 function formSubmited(e) {
     //THIS IS SO THE PAGE WAIT'S FOR US TO PRESS SUBMIT
     e.preventDefault()
-    
+    console.log(`HERE IS THE E `);
+    console.log(e);
     //THIS WILL STORE THE USER INPUT
-    let artistName = document.getElementById('header__search-input').value
-    console.log(`THE ARTIST NAME IS ${artistName}`)
-    
+    let artistName = document.getElementById('header__search-input').value    
     fetchJsonp(`https://itunes.apple.com/search?term=${artistName}&media=music&entity=album&attribute=artistTerm&limit=200​`)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
+        console.log(`THE LENGTH OF THE DATA IS ${data.resultCount}`);
+        
+        //THIS WILL CLEAR OUT THE CURRENT RESULTS WHEN YOU SEARCH FOR NEW ONES
+        let newWrapper = document.getElementById('list-wrapper')
+        newWrapper.replaceChildren()
+
         //THIS WILL DISPLAY THE RESULT COUNT
         let count = document.getElementById("result-count")   
         count.innerHTML = data.resultCount + ` results for `+artistName
         
-        //LOOP THROUGH EACH RESULT
+        //MODIFACTION FOR PAGANTION
+        let beginCounter = 0
+        //LOOP THROUGH EACH RESULT TO CREATE THE CARDS
         data.results.forEach(element => {
+        //MODIFACTION FOR PAGANTION
+            if(beginCounter<20){
 
             //CREATE A NEW CARD
             let newCard = document.createElement('div')
@@ -33,7 +43,7 @@ function formSubmited(e) {
             let newCardPic = document.createElement('img')
             
             //GIVE THE IMG THE SOURCE ADDRESS
-            newCardPic.src = element.artistViewUrl;
+            newCardPic.src = element.artworkUrl60;
 
             //CREATE THE NEW CARD NAME
             let newCardName = document.createElement('div')
@@ -51,17 +61,72 @@ function formSubmited(e) {
             newCard.appendChild(newCardPic)
             newCard.appendChild(newCardName)
             resultCard.appendChild(newCard)
-
-            // console.log(data)
-            // console.log(`The result count is ${data.resultCount}`);
-            // console.log(`The first album name is ${data.results[0].collectionName}`);
-            //data.results.forEach(element => {
-            // console.log(`The album name is ${element.collectionName}`);
-            // console.log(`The album cover is ${element.collectionViewUrl}`);
-        });
         
-    })
-  
+            beginCounter++
+        }
+        });
 
-    
+
+        //========WE ARE OUTSIDE OF THE FIRST FOREACH LOOP========
+        //=========THIS WILL ADD THE PAGANATION==========
+        let itemPerPage = 20
+        let currentPage = 1
+        let totalItems = data.resultCount
+        let numOfPages = [Math.ceil(totalItems/itemPerPage)]
+        let counter = 0
+
+        //CREATE THE PAGE BUTTONS
+        for(let i = 0; i < numOfPages; i++ ){
+            let newBtn = document.createElement('button')
+            let btnTextNode = document.createTextNode(`${i}`)
+            let parent = document.getElementById('pages')
+            newBtn.appendChild(btnTextNode)
+            newBtn.setAttribute('class','test')
+            newBtn.addEventListener('click', function(event){
+                console.log(`HERE IS THE EVENT ELEMENT`);
+                console.log(event);
+                console.log(`HERE IS THE CURRENT PAGE`);
+                console.log(i);
+                newWrapper.replaceChildren()
+                let indexStart = itemPerPage * i
+                let indexEnd = itemPerPage + indexStart
+                counter = 0
+                data.results.forEach(element =>{
+                    if(counter >= indexStart && counter < indexEnd){
+                        //CREATE A NEW CARD
+                        let newCard = document.createElement('div')
+                        
+                        //CREATE THE NEW CARD PIC
+                        let newCardPic = document.createElement('img')
+                        
+                        //GIVE THE IMG THE SOURCE ADDRESS
+                        newCardPic.src = element.artworkUrl60;
+
+                        //CREATE THE NEW CARD NAME
+                        let newCardName = document.createElement('div')
+                        
+                        //GIVE THE NEW CARD THEIR CLASS NAMES
+                        newCard.setAttribute('class','card-design')
+                        newCardPic.setAttribute('class','pic')
+                        newCardName.setAttribute('class','name')
+
+                        //CREATE NAME TEXT NODE
+                        newCardNameText = document.createTextNode(element.collectionName)
+
+                        //APPEND EVERYTHING
+                        newCardName.appendChild(newCardNameText)
+                        newCard.appendChild(newCardPic)
+                        newCard.appendChild(newCardName)
+                        resultCard.appendChild(newCard)
+                    }
+                    counter++
+                })
+
+
+            })
+            parent.appendChild(newBtn)
+            console.log(newBtn)
+            console.log(`HI`)
+        }        
+    })
 }
